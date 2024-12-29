d95b717a965a68dee8b9d5a6fae07223d5cea920
SimonGuo666 <simonguo666@qq.com>
2024-12-29 18:43:59 +0800

    Add files

---
+++ python.py
@@ -0,0 +1,21 @@
+import os
+
+def list_filenames(directory, output_file=None):
+    try:
+        filenames = os.listdir(directory)
+        if output_file:
+            with open(output_file, 'w', encoding='utf-8') as file:
+                for name in filenames:
+                    file.write('\'' +'./output/' + name +'\''+ ',' +'\n')
+            print(f"File names have been saved to {output_file}")
+        else:
+            for name in filenames:
+                print(name)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+# 修改为你的图片目录路径
+directory = "./"  # 替换为实际路径
+output_file = "filenames.txt"  # 可选，保存到文件；设置为 None 则只打印
+
+list_filenames(directory, output_file)
